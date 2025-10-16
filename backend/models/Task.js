58d8f0e96f38192f4fcd03dd3861295db404bdde
bilal iken <bilal11iken@gmail.com>
2025-10-16 19:26:56 +0100

    add task schema

---
+++ backend/models/Task.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Task title is required"],
+      trim: true,
+      minlength: [1, "Title must be at least 1 character"],
+      maxlength: [200, "Title cannot exceed 200 characters"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxlength: [1000, "Description cannot exceed 1000 characters"],
+      default: "",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    dueDate: {
+      type: Date,
+      default: null,
+    },
+    userId: {
+      type : mongoose.Schema.Types.ObjectId,
+      ref : "User",
+      required :true
+      
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+const Task = mongoose.model("Task" , taskSchema)
+
+export default Task
