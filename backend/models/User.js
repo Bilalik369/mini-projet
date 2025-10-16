a60402f5ed65cae8ac1244ae4e17583b4a408d6d
bilal iken <bilal11iken@gmail.com>
2025-10-16 14:06:41 +0100

    ajoute le user shcema

---
+++ backend/models/User.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      minlength: [2, "Full name must be at least 2 characters"],
+      maxlength: [100, "Full name cannot exceed 100 characters"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
+    },
+
+    password :{
+        type : String,
+        required : true,
+        minlength: [6, "Password must be at least 6 characters"],
+        select: false,
+        
+
+    },  phoneNumber: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+
+    address: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+
+     image: {
+      type: String,
+      default: "",
+    },
+
+}, {timestamps: true,})
+
+const User = mongoose.model("User", userSchema)
+
+export default User
