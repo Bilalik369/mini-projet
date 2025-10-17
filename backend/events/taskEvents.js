676379fc42130e6a411945017b1cc2b59f6f561e
bilal iken <bilal11iken@gmail.com>
2025-10-17 11:54:49 +0100

    inisatilisation de socket

---
+++ backend/events/taskEvents.js
@@ -0,0 +1,27 @@
+import { error } from "console"
+
+const userSockets = new Map()
+
+export const initializeSocketEvents = (io)=>{
+    io.on("connection" , (socket)=>{
+        console.log(`client connected : ${socket.id}`)
+        socket.on('authenticate' , (userId)=>{
+            if(userId){
+                userSockets.set(userId,socket.id)
+                socket.userId = userId
+                 console.log(`User ${userId} authenticated on socket ${socket.id}`)
+            }
+        })
+
+        socket.on("deconcete" , ()=>{
+            if(socket.userId){
+                userSockets.delete(socket.userId)
+                console.log(`User ${socket.userId} disconnected`)
+            }
+        })
+
+        socket.on("error" , (error)=> {
+            console.error("Socket error:", error)
+        })
+    })
+}
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,35 @@
 import express from "express"
 import dotenv from "dotenv"
+import { createServer } from "http"
+import { Server } from "socket.io"
+import cors from "cors"
 import {connectdb} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
 import taskroutes from "./routes/task.route.js"
+import {initializeSocketEvents}  from "./events/taskEvents.js"
 dotenv.config();
 
 const app = express();
+const httpServer = createServer(app)
+
+const io = new Server(httpServer , {
+    cors :{
+     origin:  "http://localhost:3000",
+     methods: ["GET", "POST", "PUT", "DELETE"],
+     credentials: true,
+    }
+})
+
 const PORT = process.env.PORT
 
 app.use(express.json())
 
+app.set("io", io)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskroutes);
 
-
+initializeSocketEvents(io) 
 
 
 connectdb()
